37060ecf3b788f8de28a1840c2795e95b48ae4e0
Soheib HDF <s.hadef@outlook.fr>
2023-01-02 14:47:46 +0100

    premiercommit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("alive and well");
+let navbar = document.getElementById("navbar");
+let battery = navigator.getBattery();
+let performance = window.performance;
+
+setInterval(function() {
+    let date = new Date();
+    let heure = date.getHours();
+    let minutes = date.getMinutes();
+    let jour = date.getDate();
+    let mois = date.getMonth() + 1;
+    let annee = date.getFullYear();
+
+    navbar.innerHTML = heure + "h" + minutes + " | " + jour + "/" + mois + "/" + annee;
+
+    battery.then(function(battery) {
+      let batteryLevel = battery.level;
+      let batteryCharging = battery.charging ? "⚡" : "";
+
+      navbar.innerHTML += " | Battery: " + Math.round(batteryLevel * 100) + "% (" + batteryCharging + ")";
+    });
+  }, 1000);
